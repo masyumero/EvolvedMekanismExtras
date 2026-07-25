6d3a8859d6cc0e18ba432aa7e4570a4cd865f27c
masyumero <167562220+masyumero@users.noreply.github.com>
2026-07-25 16:18:18 +0900

    feat(data): add advanced factory and more machine blocks to loot tables
    * Include EMExtraAdvancedFactoryBlocks in loot generation
    * Include EMExtraMoreMachineBlocks in loot generation

--- src/data/java/io/github/masyumero/emextras/datagen/common/loot/table/EMExtrasBlockLootProvider.java
+++ src/data/java/io/github/masyumero/emextras/datagen/common/loot/table/EMExtrasBlockLootProvider.java
@@ -1,5 +1,7 @@
 package io.github.masyumero.emextras.datagen.common.loot.table;
 
+import io.github.masyumero.emextras.common.integration.mekaf.regisrty.EMExtraAdvancedFactoryBlocks;
+import io.github.masyumero.emextras.common.integration.mekmm.registry.EMExtraMoreMachineBlocks;
 import io.github.masyumero.emextras.common.registry.EMExtraBlocks;
 
 public class EMExtrasBlockLootProvider extends BaseBlockLootTables {
@@ -7,5 +9,7 @@ public class EMExtrasBlockLootProvider extends BaseBlockLootTables {
     @Override
     protected void generate() {
         dropSelfWithContents(EMExtraBlocks.BLOCK.getAllBlocks());
+        dropSelfWithContents(EMExtraAdvancedFactoryBlocks.BLOCKS.getAllBlocks());
+        dropSelfWithContents(EMExtraMoreMachineBlocks.BLOCKS.getAllBlocks());
     }
 }
