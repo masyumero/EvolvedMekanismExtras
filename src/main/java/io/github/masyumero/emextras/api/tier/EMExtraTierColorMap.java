b3cc8e3b44bfd10d5275ee40cc8be16d30633888
masyumero <167562220+masyumero@users.noreply.github.com>
2026-08-19 01:50:31 +0900

    feat(tier): update color mappings to use AdvancedTier

--- src/main/java/io/github/masyumero/emextras/api/tier/EMExtraTierColorMap.java
+++ src/main/java/io/github/masyumero/emextras/api/tier/EMExtraTierColorMap.java
@@ -1,5 +1,7 @@
 package io.github.masyumero.emextras.api.tier;
 
+import com.jerry.mekextras.api.tier.AdvancedTier;
+import com.jerry.mekextras.common.tier.TierColor;
 import io.github.masyumero.emextras.common.util.EMExtraColorUtils;
 import mekanism.api.functions.FloatSupplier;
 
@@ -7,15 +9,16 @@ import java.util.function.IntSupplier;
 
 public class EMExtraTierColorMap {
 
-    private static final FloatSupplier spot = () -> {float spot = (float) ((System.currentTimeMillis() / 250) % 10) / 10;
+    private static FloatSupplier spot = () -> {
+        float spot = (float) ((System.currentTimeMillis() / 25) % 100) / 100;
         if (spot > 0.5) {
             spot = 1 - spot;
         }
         return spot * 2;
     };
 
-    public static IntSupplier absoluteOverclockedColor = () -> EMExtraColorUtils.getInterpolatedColor(new int[]{237, 238, 70}, new int[]{0, 221, 0}, spot.getAsFloat());
-    public static IntSupplier supremeQuantumColor = () -> EMExtraColorUtils.getInterpolatedColor(new int[]{166, 0, 2}, new int[]{252, 158, 250}, spot.getAsFloat());
-    public static IntSupplier cosmicDenseColor = () -> EMExtraColorUtils.getInterpolatedColor(new int[]{75, 248, 255}, new int[]{253, 245, 95}, spot.getAsFloat());
-    public static IntSupplier infiniteMultiversalColor = () -> EMExtraColorUtils.getInterpolatedColor(new int[]{247, 135, 255}, new int[]{90, 87, 90}, spot.getAsFloat());
+    public static IntSupplier absoluteOverclockedColor = () -> EMExtraColorUtils.getInterpolatedColor(TierColor.getRgb(AdvancedTier.ABSOLUTE), new int[]{0, 221, 0}, spot.getAsFloat());
+    public static IntSupplier supremeQuantumColor = () -> EMExtraColorUtils.getInterpolatedColor(TierColor.getRgb(AdvancedTier.SUPREME), new int[]{252, 158, 250}, spot.getAsFloat());
+    public static IntSupplier cosmicDenseColor = () -> EMExtraColorUtils.getInterpolatedColor(TierColor.getRgb(AdvancedTier.COSMIC), new int[]{253, 245, 95}, spot.getAsFloat());
+    public static IntSupplier infiniteMultiversalColor = () -> EMExtraColorUtils.getInterpolatedColor(TierColor.getRgb(AdvancedTier.INFINITE), new int[]{90, 87, 90}, spot.getAsFloat());
 }
